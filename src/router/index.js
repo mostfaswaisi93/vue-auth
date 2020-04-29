07fc60bdfa5c4e926ecc4b3adecc5403c9d5648f
mostfaswaisi93 <mostfaswaisi@gmail.com>
2020-04-29 17:02:38 +0300

    Login - Register Files

--- src/router/index.js
+++ src/router/index.js
@@ -1,30 +1,70 @@
 import Vue from "vue";
-import VueRouter from "vue-router";
+import Router from "vue-router";
 import Home from "../views/Home.vue";
+import Login from "../views/Login";
+import Register from "../views/Register";
+import Profile from "../views/Profile";
 
-Vue.use(VueRouter);
+Vue.use(Router);
 
-const routes = [
-  {
-    path: "/",
-    name: "Home",
-    component: Home
-  },
-  {
-    path: "/about",
-    name: "About",
-    // route level code-splitting
-    // this generates a separate chunk (about.[hash].js) for this route
-    // which is lazy-loaded when the route is visited.
-    component: () =>
-      import(/* webpackChunkName: "about" */ "../views/About.vue")
-  }
-];
+const router = new Router({
+    mode: "history",
+    base: process.env.BASE_URL,
+    routes: [{
+            path: "/",
+            name: "home",
+            component: Home
+        },
+        {
+            path: "/login",
+            name: "login",
+            component: Login,
+            meta: {
+                guest: true
+            }
+        },
+        {
+            path: "/register",
+            name: "register",
+            component: Register,
+            meta: {
+                guest: true
+            }
+        },
+        {
+            path: "/profile",
+            name: "profile",
+            component: Profile,
+            meta: {
+                secure: true
+            }
+        }
+    ]
+});
 
-const router = new VueRouter({
-  mode: "history",
-  base: process.env.BASE_URL,
-  routes
+router.beforeEach((to, from, next) => {
+    if (to.matched.some(record => record.meta.secure)) {
+        // if no token
+        if (localStorage.getItem("token") == null) {
+            console.log("no token");
+            next({
+                path: "/login"
+            });
+        } else {
+            next();
+        }
+    } else if (to.matched.some(record => record.meta.guest)) {
+        if (localStorage.getItem("token") == null) {
+            next();
+        } else {
+            console.log("no token");
+            next({
+                path: "/profile"
+            });
+        }
+    } else {
+        next();
+    }
 });
 
 export default router;
---
+++ src/views/Login.vue
@@ -0,0 +1,70 @@
+<template>
+  <div class="login">
+    <div class>
+      <div class="col-md-6 offset-md-3">
+        <h2 class="display-4 text-center mt-5">Login Form</h2>
+
+        <form action="#">
+          <div class="form-group">
+            <label for="email">Email</label>
+            <input type="text" name="email" id="email" v-model="email" class="form-control" />
+          </div>
+
+          <div class="form-group">
+            <label for="password">Password</label>
+            <input
+              type="password"
+              name="password"
+              id="password"
+              v-model="password"
+              class="form-control"
+            />
+            <div style="color:red;" v-if="error">{{ error }}</div>
+          </div>
+
+          <div class="form-group">
+            <button type="submit" class="btn btn-info btn-block" @click.prevent="performLogin">Login</button>
+          </div>
+        </form>
+      </div>
+    </div>
+  </div>
+</template>
+
+<script>
+import axios from "axios";
+export default {
+  name: "login",
+  data() {
+    return {
+      email: "",
+      password: "",
+      error: ""
+    };
+  },
+  methods: {
+    performLogin() {
+      axios
+        .post("http://localhost:8000/api/auth/login", {
+          email: this.email,
+          password: this.password
+        })
+        .then(res => {
+          console.log(res.data);
+          // store the token and user in localstorage
+          // const token = localStorage.setItem("token", res.data.access_token)
+          // const user = localStorage.setItem("user", res.data.user)
+          this.$router.push("/profile");
+        })
+        .catch(err => {
+          console.log(err.message);
+          this.error = err.message;
+        });
+      console.log("perfom login");
+    }
+  }
+};
+</script>
+
+<style>
+</style>
